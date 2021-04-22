c4f38e4010e2d75fa857a928dc3c102d5f9149ee
Lauren Arnett <lba2138@columbia.edu>
2021-04-22 17:01:58 -0400

    clean up simd_sum

--- numba-simd/simd_sum.py
+++ numba-simd/simd_sum.py
@@ -2,14 +2,28 @@ import numpy as np
 from numba import jit
 import timeit
 
-import ctypes
-
-import llvmlite.binding as llvm
-llvm.set_option('', '--debug-only=loop-vectorize')
-
-pmb = PassManagerBuilder.populate(loop_vectorize=False)
+NUM_ITERS = 1000
 
 
+# ******************************************************************************
+#
+# Testing utilities
+#
+# ******************************************************************************
+def run_test(fn, arg1, label, show_instr=False):
+    if 'simd' in fn.__name__:
+        print("JIT Compiling {}".format(label))
+        # must run at least one time to JIT compile the function
+        t = timeit.timeit('{}({})'.format(fn.__name__, arg1),"from __main__ import {}, {}".format(fn.__name__, arg1), number=1)
+
+    print("Running %s" % label)
+    t = timeit.timeit('{}({})'.format(fn.__name__, arg1),"from __main__ import {}, {}".format(fn.__name__, arg1) ,number=NUM_ITERS)
+    print("Time per iteration: {} s".format(t / NUM_ITERS))
+    if 'simd' in fn.__name__ and show_instr == True:
+        print("{} instructions".format(label))
+        find_instr(fn, keyword='xmm', sig=0)
+        find_instr(fn, keyword='ymm', sig=0)
+    print("")
 
 def find_instr(func, keyword, sig=0, limit=5):
     count = 0
@@ -20,34 +34,26 @@ def find_instr(func, keyword, sig=0, limit=5):
             if count >= limit:
                 break
     if count == 0:
-        print('No instructions found')
+        print('No %s instructions found' % keyword)
+
+# ******************************************************************************
 
 vals = np.random.randint(0, 500, ((1 << 16) + 10), dtype=np.uint32)
-
-NUM_REPEATS = 10
 
 '''
 Sum values in vanilla python with a for loop
 '''
-print("***********************************")
-print("SUM")
-print("***********************************")
 def vals_sum(x):
     res = 0
     for i in range(x.shape[0]):
         res += x[i]
     return res
 
-print("vals sum")
-t1 = timeit.timeit('vals_sum(vals)',"from __main__ import vals_sum, vals", number=1000)
-print("vals sum {}".format(t1 / NUM_REPEATS))
+run_test(vals_sum, 'vals', 'vanilla sum with for loop')
 
 '''
 Sum values in vanilla python with a for loop and a filter
 '''
-print("***********************************")
-print("SUM FILTER")
-print("***********************************")
 def vals_sum_filter(x):
     res = 0
     for i in range(x.shape[0]):
@@ -56,47 +62,27 @@ def vals_sum_filter(x):
             res += val
     return res
 
-print("vals sum filter")
-# t1 = timeit.timeit('vals_sum_filter(vals)',"from __main__ import vals_sum_filter, vals", number=NUM_REPEATS)
-# print("vals sum filter {}".format(t1 / NUM_REPEATS))
-
+run_test(vals_sum_filter, 'vals', 'vanilla sum with for loop & filter')
 
 '''
 Sum values using numpy
 '''
-print("***********************************")
-print("SUM NP")
-print("***********************************")
 def vals_sum_np(x):
     return np.sum(x)
 
-print("sum np")
-t1 = timeit.timeit('vals_sum_np(vals)',"from __main__ import vals_sum_np, vals", number=NUM_REPEATS)
-print("sum np {}".format(t1 / NUM_REPEATS))
+run_test(vals_sum_np, 'vals', 'np.sum()')
 
 '''
 Sum values using numpy & where
 '''
-print("***********************************")
-print("SUM NP FILTER")
-print("***********************************")
 def vals_sum_filter_np(x):
-    return np.sum(np.where(x > 127, x, 0))
+    return np.sum(np.where(x > 100, x, 0))
 
-print("sum np filter")
-t1 = timeit.timeit('vals_sum_filter_np(vals)',"from __main__ import vals_sum_filter_np, vals", number=NUM_REPEATS)
-print("sum np filter {}".format(t1 / NUM_REPEATS))
-
-
+run_test(vals_sum_filter_np, 'vals', 'np.sum() with np.where()')
 
 '''
 array sum using simd with a for loop
 '''
-print("***********************************")
-print("SIMD SUM")
-print("***********************************")
-
-
 @jit(nopython=True)
 def simd_sum(x):
     res = 0
@@ -104,84 +90,36 @@ def simd_sum(x):
         res += x[i]
     return res
 
-print("simd sum precompile")
-t1 = timeit.timeit('simd_sum(vals)',"from __main__ import simd_sum, vals",number=NUM_REPEATS)
-print("pre compile simd sum {}".format(t1 / NUM_REPEATS))
-
-print("Actual simd sum")
-t1 = timeit.timeit('simd_sum(vals)',"from __main__ import simd_sum, vals", number=NUM_REPEATS)
-print("Actual simd sum {}".format(t1 / NUM_REPEATS))
-
-find_instr(simd_sum, keyword='ymm', sig=0)
-find_instr(simd_sum, keyword='xmm', sig=0)
-
+run_test(simd_sum, 'vals', 'numba simd with for loop')
 
 '''
 array sum using simd with numpy.sum()
 '''
-print("***********************************")
-print("SIMD SUM NP")
-print("***********************************")
 @jit(nopython=True)
 def simd_sum_np(x):
     return np.sum(x)
 
-print("simd sum np precompile")
-t1 = timeit.timeit('simd_sum_np(vals)',"from __main__ import simd_sum_np, vals", number=NUM_REPEATS)
-print("pre compile simd sum np {}".format(t1 / NUM_REPEATS))
-
-print("Actual simd np sum")
-t1 = timeit.timeit('simd_sum_np(vals)',"from __main__ import simd_sum_np, vals", number=NUM_REPEATS)
-print("Actual simd sum np {}".format(t1 / NUM_REPEATS))
-
-find_instr(simd_sum_np, keyword='ymm', sig=0)
-find_instr(simd_sum_np, keyword='xmm', sig=0)
-
-
+run_test(simd_sum_np, 'vals', 'numba simd with np.sum()')
 
 '''
 array sum using simd with a for loop and a filter
 '''
-print("***********************************")
-print("SIMD SUM FILTER")
-print("***********************************")
 @jit(nopython=True)
 def simd_sum_filter(x):
     res = 0
     for i in range(x.shape[0]):
         val = x[i]
-        if val >= 128:
+        if val >= 100:
             res += val
     return res
 
-print("simd sum filter precompile")
-t2 = timeit.timeit('simd_sum_filter(vals)',"from __main__ import simd_sum_filter, vals", number = NUM_REPEATS)
-print("pre compile simd sum filter {}".format(t2 / NUM_REPEATS))
-
-print("Actual simd sum filter")
-t2 = timeit.timeit('simd_sum_filter(vals)',"from __main__ import simd_sum_filter, vals",number=NUM_REPEATS)
-print("Actual simd sum filter {}".format(t2 / NUM_REPEATS))
-
-find_instr(simd_sum_filter, keyword='ymm', sig=0)
-find_instr(simd_sum_filter, keyword='xmm', sig=0)
+run_test(simd_sum_filter, 'vals', 'numba simd with for loop & filter')
 
 '''
 array sum using simd with numpy.sum() and numpy.where() filter
 '''
-print("***********************************")
-print("SIMD SUM FILTER NP")
-print("***********************************")
 @jit(nopython=True)
 def simd_sum_filter_np(x):
     return np.sum(np.where(x > 127, x, 0))
-print("simd sum filter np pre compile")
-t2 = timeit.timeit('simd_sum_filter_np(vals)',"from __main__ import simd_sum_filter_np, vals",number=NUM_REPEATS)
-print("simd sum filter np precompile {}".format(t2 / NUM_REPEATS))
 
-print("Actual simd sum filter np")
-t2 = timeit.timeit('simd_sum_filter(vals)',"from __main__ import simd_sum_filter, vals", number=NUM_REPEATS)
-print("Actual simd sum filter np {}".format(t2 / NUM_REPEATS))
-
-find_instr(simd_sum_filter_np, keyword='ymm', sig=0)
-find_instr(simd_sum_filter_np, keyword='xmm', sig=0)
-
+run_test(simd_sum_filter, 'vals', 'numba simd with np.sum() and np.where()')
